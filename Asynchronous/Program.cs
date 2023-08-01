087bb63a8a12c4d357fb290a7189afb0a619150a
Edoghotu Gideon Azibaobuom <gideon.edoghotu@cyphercrescent.com>
2023-08-01 09:00:03 +0100

    Add project files.

---
+++ Asynchronous/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+
+Task.Run(() => Enumerable.Range(1, 20).ToList().ForEach(x => Console.WriteLine(x)));
+
+Task.Run(() => Enumerable.Range(1, 10).ToList().ForEach(x => Console.WriteLine("Processing another thing")));
+
+
+Console.ReadKey();
