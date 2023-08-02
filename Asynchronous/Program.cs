6b3bf1a7a5f500d9883566e1b0034f8bee16538f
Edoghotu Gideon Azibaobuom <gideon.edoghotu@cyphercrescent.com>
2023-08-02 16:43:16 +0100

    updated

--- Asynchronous/Program.cs
+++ Asynchronous/Program.cs
@@ -1,11 +1,53 @@
 ﻿// See https://aka.ms/new-console-template for more information
-//Console.WriteLine("Hello, World!");
 
+//var lines = File.ReadAllLines("mytxt.txt");
 
+//foreach (var line in lines)
+//{
+//    Console.WriteLine(lines);
+//}
 
-Task.Run(() => Enumerable.Range(1, 20).ToList().ForEach(x => Console.WriteLine(x)));
+Console.WriteLine("starting...");
 
-Task.Run(() => Enumerable.Range(1, 10).ToList().ForEach(x => Console.WriteLine("Processing another thing")));
+async Task ReadFile()
+{
+    Console.WriteLine("executing...");
+    var lines = await File.ReadAllLinesAsync("mytxt.txt");
 
+    foreach (var line in lines)
+    {
+        Console.WriteLine(line);
+    }
+    
+}
 
-Console.ReadKey();
+async Task<int> GetDataAsync()
+{
+    // Simulate a network call
+    await Task.Delay(5000);
+    var res = 42;
+    Console.WriteLine(res);
+    return res;
+}
+
+//var e = ReadFile();
+var res =  GetDataAsync();
+
+//File.ReadAllLinesAsync("mytxt.txt").ContinueWith(x =>
+//{
+//    if (x.IsFaulted)
+//    {
+//        Console.Error.WriteLine(x.Exception);
+//        return;
+//    }
+//    var lines = x.Result;
+
+//    foreach (var line in lines)
+//    {
+//        Console.WriteLine(line);
+//    }
+//});
+
+//Console.ReadKey();
+Console.WriteLine("completed");
+Console.ReadLine();
