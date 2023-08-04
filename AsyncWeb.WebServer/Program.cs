4eca1881e2aa7f323f651b54c75fc43e7e3b4ab5
Edoghotu Gideon Azibaobuom <gideon.edoghotu@cyphercrescent.com>
2023-08-04 17:49:37 +0100

    updated

--- AsyncWeb.WebServer/Program.cs
+++ AsyncWeb.WebServer/Program.cs
@@ -1,23 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseRouting();
+app.UseEndpoints(endpoints =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseAuthorization();
-
-app.MapControllers();
+    endpoints.MapGet("/yes", async context =>
+    {
+        await context.Response.WriteAsync("Hello Worlssd");
+    });
+});
 
 app.Run();
